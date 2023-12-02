ff3f1d9c53f14f6fddb3fb976bc6544fde7e99e3
Anastasia <onastja02@gmail.com>
2023-12-02 16:49:10 +0300

    первая часть

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use("/static", express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs")
+
+// get method
+app.get('/',(req, res) => {
+    res.render("todo.ejs");
+});
+
+// post method
+app.post("/", (req, res) =>{
+    console.log(req.body);
+})
+
+app.listen(3000, () => console.log("Server Up and running"));
