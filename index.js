82846f6bce7a0b706b2e3be0e4303391cb6db3b4
Anastasia <onastja02@gmail.com>
2023-12-09 18:39:57 +0300

    вторая часть (исправление коннекта с базой данных)

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ app.use("/static", express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
 // connection to database
-mongoose.set("useFindAndModify", true);
-mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true}, () => {
-    console.log("Connected to db!");
+// mongoose.set("useFindAndModify", false);
+// mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+//   console.log("Connected to db!");
+//   app.listen(3000, () => console.log("Server Up and running"));
+// });
 
-    app.listen(3000, () => console.log("server Up and running"));
-})
+async function run() {
+    await mongoose.connect(process.env.DB_CONNECT, {});
+    console.log(mongoose.connection.readyState);
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+  }
+  run();
 
 app.set("view engine", "ejs");
 
